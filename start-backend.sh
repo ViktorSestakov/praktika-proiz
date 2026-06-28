77179b4e9a54b4ce63cb92836d65432b55d2d187
BNKTOP IIIecTakoB <viktorsestakov431@gmail.com>
2026-06-28 17:51:13 +0500

    Update start-backend.sh
    Убраны не нужные строки

--- start-backend.sh
+++ start-backend.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
-
-# Запуск Backend на Linux/Mac
-
-echo "🔧 Запуск Backend (Flask)..."
+echo "Запуск бэкэнд!"
 echo ""
 
 cd backend
 
 # Проверяем есть ли venv
 if [ ! -d "venv" ]; then
-    echo "📦 Создаю виртуальное окружение..."
+    echo "Создание виртуального окружения..."
     python3 -m venv venv
 fi
-
-# Активируем venv
-source venv/bin/activate
-
-# Устанавливаем зависимости
-echo "📚 Установка зависимостей..."
-pip install -r requirements.txt
-
-echo ""
-echo "✅ Backend запущен на: http://localhostt:5000"
-echo ""
 
 # Запускаем приложение
 python app.py
